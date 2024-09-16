50a26ab83ec6413d3b13436da61ae26d7843ab0a
Mihai Rusu <mrusu@2-B-9.42heilbronn.de>
2024-09-16 16:38:35 +0200

    ex01

--- ex01/inc/ScavTrap.hpp
+++ ex01/inc/ScavTrap.hpp
@@ -1,34 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   ScavTrap.hpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mrusu <mrusu@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/09/16 12:59:58 by mrusu             #+#    #+#             */
-/*   Updated: 2024/09/16 15:56:16 by mrusu            ###   ########.fr       */
+/*   Created: 2024/09/16 15:09:14 by mrusu             #+#    #+#             */
+/*   Updated: 2024/09/16 16:00:10 by mrusu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../inc/ClapTrap.hpp"
+#pragma once
+#include "ClapTrap.hpp"
 
-int main()
+class ScavTrap : public ClapTrap
 {
-	ClapTrap fool1("Fool1");
-	ClapTrap fool2("Fool2");
-	ClapTrap mfool;
+	public:
+		ScavTrap();
+		ScavTrap(const std::string& name);
+		ScavTrap(const ScavTrap& other); // copy constructor
+		~ScavTrap();
+		ScavTrap& operator=(const ScavTrap& other);
 
-	fool1.attack("Fool2");
-	fool2.takeDamage(5);
-	fool2.beRepaired(15);
-	
-	fool2.attack("Fool1");
-	fool1.takeDamage(5);
-	fool1.beRepaired(3);
-	
-	mfool.attack("Fool2");
-	fool2.takeDamage(50);
-	fool2.attack("Fool1");
-	
-	return (0);
-}
+		void	attack(const std::string& target);
+		void	guardGate();
+
+	private:
+};
--- ex01/srcs/main.cpp
+++ ex01/srcs/main.cpp
@@ -6,29 +6,21 @@
 /*   By: mrusu <mrusu@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/16 12:59:58 by mrusu             #+#    #+#             */
-/*   Updated: 2024/09/16 15:56:16 by mrusu            ###   ########.fr       */
+/*   Updated: 2024/09/16 16:19:48 by mrusu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/ClapTrap.hpp"
+#include "../inc/ScavTrap.hpp"
 
 int main()
 {
-	ClapTrap fool1("Fool1");
-	ClapTrap fool2("Fool2");
-	ClapTrap mfool;
+	ScavTrap fool1("Fool1");
 
-	fool1.attack("Fool2");
-	fool2.takeDamage(5);
-	fool2.beRepaired(15);
+	fool1.attack("target");
 	
-	fool2.attack("Fool1");
-	fool1.takeDamage(5);
-	fool1.beRepaired(3);
+	fool1.guardGate();
 	
-	mfool.attack("Fool2");
-	fool2.takeDamage(50);
-	fool2.attack("Fool1");
-	
+
 	return (0);
 }
